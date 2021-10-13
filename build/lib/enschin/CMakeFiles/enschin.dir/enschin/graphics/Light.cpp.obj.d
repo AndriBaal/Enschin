2d7277a6097f481a99a3a5bc2d804d1b1bfc6ba9
Andri <80914617+Shuray04@users.noreply.github.com>
2021-10-14 01:29:15 +0200

    [CLEAN] Cleanup

--- build/lib/enschin/CMakeFiles/enschin.dir/enschin/graphics/Light.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-lib/enschin/CMakeFiles/enschin.dir/enschin/graphics/Light.cpp.obj: \
- C:/Users/andri/Documents/GitHub/CMake-Engine/lib/enschin/enschin/graphics/Light.cpp
--- build/lib/enschin/CMakeFiles/enschin.dir/enschin/math/Dimension.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-lib/enschin/CMakeFiles/enschin.dir/enschin/math/Dimension.cpp.obj: \
- C:/Users/andri/Documents/GitHub/CMake-Engine/lib/enschin/enschin/math/Dimension.cpp \
- C:/Users/andri/Documents/GitHub/CMake-Engine/lib/enschin/enschin/math/Dimension.h
