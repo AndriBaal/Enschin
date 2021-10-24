e00d434d1cf0e5696e79ff88ef44316c868b9d51
Andri <80914617+Shuray04@users.noreply.github.com>
2021-10-24 03:33:24 +0200

    [FIX] Removed build files

--- build/lib/enschin/CMakeFiles/enschin.dir/enschin/math/Dim.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-lib/enschin/CMakeFiles/enschin.dir/enschin/math/Dim.cpp.obj: \
- C:/Users/andri/Documents/GitHub/CMake-Engine/lib/enschin/enschin/math/Dim.cpp \
- C:/Users/andri/Documents/GitHub/CMake-Engine/lib/enschin/enschin/math/Dim.h
--- build/lib/enschin/CMakeFiles/enschin.dir/enschin/math/Vec2.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-lib/enschin/CMakeFiles/enschin.dir/enschin/math/Vec2.cpp.obj: \
- C:/Users/andri/Documents/GitHub/CMake-Engine/lib/enschin/enschin/math/Vec2.cpp \
- C:/Users/andri/Documents/GitHub/CMake-Engine/lib/enschin/enschin/math/Vec2.h
