997dc9bef0c68f313da5eaf70c5813c000644860
Andri <80914617+Shuray04@users.noreply.github.com>
2021-10-24 16:16:43 +0200

    [UPDATE] Rotation working

--- build/lib/enschin/CMakeFiles/enschin.dir/enschin/math/Vec3.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-lib/enschin/CMakeFiles/enschin.dir/enschin/math/Vec3.cpp.obj: \
- C:/Users/andri/Documents/GitHub/CMake-Engine/lib/enschin/enschin/math/Vec3.cpp \
- C:/Users/andri/Documents/GitHub/CMake-Engine/lib/enschin/enschin/math/Vec3.h
