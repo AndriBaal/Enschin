67f77cdf761eccdd482780704ee2112f57a71551
Andri <80914617+Shuray04@users.noreply.github.com>
2021-10-28 19:50:21 +0200

    [FIX] Fixed capitalization

--- build/lib/enschin/CMakeFiles/enschin.dir/enschin/settings.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-lib/enschin/CMakeFiles/enschin.dir/enschin/settings.cpp.obj: \
- C:\Users\Andri\ Baal\Documents\GitHub\CMake-engine\lib\enschin\enschin\settings.cpp
--- build/lib/enschin/CMakeFiles/enschin.dir/enschin/util/timer.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-lib/enschin/CMakeFiles/enschin.dir/enschin/util/timer.cpp.obj: \
- C:\Users\Andri\ Baal\Documents\GitHub\CMake-engine\lib\enschin\enschin\util\timer.cpp
