fe99ccf63cc7faa4b115de8a75417df5edcfa9a9
Andri <80914617+Shuray04@users.noreply.github.com>
2021-11-02 00:57:41 +0100

    [FIX] Small optimizations

--- build/lib/enschin/CMakeFiles/enschin.dir/enschin/settings.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-lib/enschin/CMakeFiles/enschin.dir/enschin/settings.cpp.obj: \
- C:/Users/andri/Documents/GitHub/CMake-Engine/lib/enschin/enschin/settings.cpp
--- build/lib/enschin/CMakeFiles/enschin.dir/enschin/util/timer.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-lib/enschin/CMakeFiles/enschin.dir/enschin/util/timer.cpp.obj: \
- C:/Users/andri/Documents/GitHub/CMake-Engine/lib/enschin/enschin/util/timer.cpp
